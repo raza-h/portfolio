f801ea870fe364df69432a6900edf8f1f9ea3572
raza-h <razah12145@gmail.com>
2025-08-24 14:33:41 +0500

    unneeded change removed from tech list

--- components/tech-list/TechList.tsx
+++ components/tech-list/TechList.tsx
@@ -6,7 +6,7 @@ import { TechListProps } from "@/types";
 
 const TechList: FC<TechListProps> = ({ list = [] }) => {
   return (
-    <HStack w={"100%"} flexWrap={"wrap"} pos={'relative'}>
+    <HStack w={"100%"} flexWrap={"wrap"}>
       {list?.map((technology) => (
         <TechChip key={technology} technology={TECH_CHIP[technology]} />
       ))}
